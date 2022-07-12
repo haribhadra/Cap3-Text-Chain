cf20e8cae50a8774e1d78ed5f9a373e96ce371f3
Hector <dharmadns@gmail.com>
2022-07-12 16:56:46 -0300

    Ejercicio Pagina 68

---
+++ EjerPag68.py
@@ -0,0 +1,36 @@
+print()
+text= 'Mi primera daceda de texto'
+print(text)
+print()
+
+# Usando comillas "" o '', caracteres de escape '/', etc
+comillas = 'Los llamados "strings" son secuencias de caracteres'
+print(comillas)
+print()
+
+# Comillas simples escapadas con "/" y con comillas simples
+comillas2 = 'Mas texto con escapes de '\strings'\ como sub cadenas'
+print(comillas2)
+print()
+
+# El uso de comillas triples ''' '''
+poema = ''' Todavía tengo casi todos mis dientes
+casi todos mis cabellos y poquísimas canas
+puedo hacer y deshacer el amor
+trepar una escalera de dos en dos
+y correr cuarenta metros detrás del ómnibus
+o sea que no debería sentirme viejo
+pero el grave problema es que antes
+no me fijaba en estos detalles.'''
+print(poema)
+print()
+
+# Dentro de las cadenas de texto se puede usar la "conversión" de cualquier valor a un string.
+conver1 = str(True)
+print(conver1)
+print()
+
+# Secuencias de escape \n (salto de linea) o \t (tabulación)
+salto = 'Texto con sltos de varias lineas \nSegunda linea de texto y van mas \nTercera linea de texto'
+print(salto)
+print()
